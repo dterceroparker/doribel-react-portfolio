99f46c6c2d4a35c9a9120ad02bb1be8ef8327ec3
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-06-20 16:10:29 -0400

    update About page with span sr-only for screen readers

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import AboutItem from '../components/AboutItem';
+import React from 'react'
+import AboutItem from '../components/AboutItem'
 
 const data = [
   {
     year: 2024,
     title: 'General Assembly Software Engineering Bootcamp',
-    details: 'Lorem Ipsum',
+    details: 'Replace with your first experience details here',
   },
   {
-    year: 2024,
-    title: 'General Assembly Software Engineering Bootcamp',
-    details: 'Lorem Ipsum',
+    year: 2023, 
+    title: 'Another Experience Title',
+    details: 'Replace with your second experience details here',
   },
 ]
 
-const About = () => { 
+const About = () => {
   return (
     <div id="about" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
-      <h1 className="text-4xl font-bold text-center text-[#001b5e]">About</h1>
+      <h1 className="text-4xl font-bold text-center text-[#001b5e]">
+        <span className="sr-only">About</span> About Me
+      </h1>
       {data.map((item, idx) => (
         <AboutItem key={idx} year={item.year} title={item.title} details={item.details} />
       ))}
