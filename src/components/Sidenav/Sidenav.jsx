8c1211393bb81428551699179d377bd621fbd2ad
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-06-20 11:41:12 -0400

    update a tags href

--- src/components/Sidenav/Sidenav.jsx
+++ src/components/Sidenav/Sidenav.jsx
@@ -25,12 +25,12 @@ const Sidenav = () => {
               <AiOutlineHome size={20}/>
               <span className='pl-14'>Home</span>
             </a>
-            <a href="#main" 
+            <a href="#about" 
             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
               <AiOutlineInfoCircle size={20}/>
               <span className='pl-14'>About</span>
             </a>
-            <a href="#main" 
+            <a href="#projects" 
             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
               <AiOutlineProject size={20}/>
               <span className='pl-14'>Projects</span>
@@ -40,7 +40,7 @@ const Sidenav = () => {
               <BsPerson size={20}/>
               <span className='pl-14'>Resume</span>
             </a>
-            <a href="#main" 
+            <a href="#contact" 
             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
               <AiOutlineMail size={20}/>
               <span className='pl-14'>Contact</span>
@@ -48,11 +48,27 @@ const Sidenav = () => {
           </div>
         )
         : (
-          <div></div>
-
-        )
-      }
-
+          ''
+        )}
+        <div className='md:block hidden fixed top-[25%] z-10'>
+          <div className='flex flex-col'>
+            <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <AiOutlineHome size={20} />
+            </a>
+            <a href="#about" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <AiOutlineInfoCircle size={20} />
+            </a>
+            <a href="#projects" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <AiOutlineProject size={20} />
+            </a>
+            <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <BsPerson size={20} />
+            </a>
+            <a href="#contact" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <AiOutlineMail size={20} />
+            </a>
+          </div>
+        </div>
 
 
     </div>
