fa00c0268dc63d92964404a0329f6337e7946d2f
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-06-20 15:42:42 -0400

    check AboutItems component for errors and update year color

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,8 +1,12 @@
 
 import  React, { useState } from 'react'
 
-import {  AiOutlineMenu, AiOutlineHome, AiOutlineProject, AiOutlineMail, AiOutlineInfoCircle } from 'react-icons/ai'
-// import { GrProjects } from 'react-icons/gr'
+import {  
+  AiOutlineMenu, 
+  AiOutlineHome, 
+  AiOutlineProject, 
+  AiOutlineMail, 
+  AiOutlineInfoCircle } from 'react-icons/ai'
 import {BsPerson} from 'react-icons/bs'
 
 const Sidenav = () => {
@@ -20,27 +24,32 @@ const Sidenav = () => {
       {
         nav ? (
           <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
-            <a href="#main" 
+            <a onClick={handleNav}
+            href="#main" 
             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
               <AiOutlineHome size={20}/>
               <span className='pl-14'>Home</span>
             </a>
-            <a href="#about" 
+            <a onClick={handleNav}
+            href="#about" 
             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
               <AiOutlineInfoCircle size={20}/>
               <span className='pl-14'>About</span>
             </a>
-            <a href="#projects" 
+            <a onClick={handleNav}
+            href="#projects" 
             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
               <AiOutlineProject size={20}/>
               <span className='pl-14'>Projects</span>
             </a>
-            <a href="#main" 
+            <a onClick={handleNav}
+            href="#main" 
             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
               <BsPerson size={20}/>
               <span className='pl-14'>Resume</span>
             </a>
-            <a href="#contact" 
+            <a onClick={handleNav}
+            href="#contact" 
             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
               <AiOutlineMail size={20}/>
               <span className='pl-14'>Contact</span>
