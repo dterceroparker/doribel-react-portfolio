49c259e21ea014f9a2358a966caaab8005313093
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-06-20 11:26:52 -0400

    add About, Projects, Resume and Contact icons

--- src/components/Sidenav/Sidenav.jsx
+++ src/components/Sidenav/Sidenav.jsx
@@ -1,7 +1,9 @@
 
 import  React, { useState } from 'react'
 
-import {  AiOutlineMenu, AiOutlineHome } from 'react-icons/ai'
+import {  AiOutlineMenu, AiOutlineHome, AiOutlineProject, AiOutlineMail, AiOutlineInfoCircle } from 'react-icons/ai'
+// import { GrProjects } from 'react-icons/gr'
+import {BsPerson} from 'react-icons/bs'
 
 const Sidenav = () => {
   const [nav, setNav] = useState(false)
@@ -18,9 +20,30 @@ const Sidenav = () => {
       {
         nav ? (
           <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
-            <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+            <a href="#main" 
+            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
               <AiOutlineHome size={20}/>
               <span className='pl-14'>Home</span>
+            </a>
+            <a href="#main" 
+            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+              <AiOutlineInfoCircle size={20}/>
+              <span className='pl-14'>About</span>
+            </a>
+            <a href="#main" 
+            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+              <AiOutlineProject size={20}/>
+              <span className='pl-14'>Projects</span>
+            </a>
+            <a href="#main" 
+            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+              <BsPerson size={20}/>
+              <span className='pl-14'>Resume</span>
+            </a>
+            <a href="#main" 
+            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+              <AiOutlineMail size={20}/>
+              <span className='pl-14'>Contact</span>
             </a>
           </div>
         )
