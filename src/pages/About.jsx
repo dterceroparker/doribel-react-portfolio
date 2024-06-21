234b825991e9f06a3c5cc1dcf5fcba92923d9e39
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-06-21 16:34:58 -0400

    add logo to About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -20,14 +20,17 @@ const data = [
 
 function About() {
   return (
-    <div id="about" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+    <section id="about" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+      <div className="flex flex-col md:flex-row justify-center items-center gap-8">
+        <img src="public/logo.png" alt="Logo" className="w-49 h-49 rounded-full object-cover mx-auto md:mx-0" />
+      </div>
       <h1 className="text-4xl font-bold text-center text-[#001b5e]">
         <span className="sr-only">About</span> About Me
       </h1>
       {data.map((item, idx) => (
         <AboutItem key={idx} year={item.year} title={item.title} details={item.details} />
       ))}
-    </div>
+    </section>
   )
 }
 
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,7 +4,9 @@ import { FaLinkedinIn } from 'react-icons/fa'
 function Landing() {
   return (
     <div id='main'>
-      <img className='w-full h-screen object-cover object-left' src="src/assets/images/landing.jpg" alt="landing page background" />
+      <img className='w-full h-screen object-cover object-left' 
+      src="src/assets/images/landing.jpg" 
+      alt="landing page background" />
       <div className='w-full h-screen absolute top-0 left-0 bg-white/40'>
         <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
           <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>Welcome! I'm Doribel</h1>
@@ -28,7 +30,7 @@ function Landing() {
             />
           </h2>
           <div 
-            className='linkedin-icon border border-linkedin hover:border-yellow-500 rounded-full p-2 transition duration-300 ease-in-out'
+            className='linkedin-icon border border-linkedin hover:border-yellow-500 rounded-full p-2 transition duration-300 ease-in-out flex justify-center items-center'
             onClick={() => window.open('https://www.linkedin.com/in/doribelt-p')}>  
             <FaLinkedinIn 
               className='text-linkedin hover:text-linkedin-hover' size={30} 
