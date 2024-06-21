adefd7c43ff19e90ea229fab409db9f8c3f6d308
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-06-21 19:24:12 -0400

    update landing landing image path

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,12 +5,12 @@ function Landing() {
   return (
     <div id='main'>
       <img className='w-full h-screen object-cover object-left' 
-      src="src/assets/images/landing.jpg" 
+      src="/landing.jpg" 
       alt="landing page background" />
       <div className='w-full h-screen absolute top-0 left-0 bg-white/40'>
         <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
           <h1 
-            className='sm:text-5xl text-4xl font-bold text-gray-800'>Welcome! I am Doribel!
+            className='flex text-center sm:text-5xl text-4xl font-bold text-gray-800'>Welcome! I am Doribel!
           </h1>
           <h2 
             className='flex text-center sm:text-3xl text-2xl pt-4 py-4 text-gray-800'>
