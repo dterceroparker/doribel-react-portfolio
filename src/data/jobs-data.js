f9dc7ea9ffeddd6d189cd5160ad40dc6637df1b4
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-06-22 10:19:21 -0400

    create data jobs-data to organize code destructured jobs object in About component

---
+++ src/data/jobs-data.js
@@ -0,0 +1,17 @@
+export const jobs = [
+  {
+    year: 2024,
+    title: '',
+    details: 'I am interested in the intersection of technology accessibility and inclusive growth, where innovative solutions empower all users and bridge the digital divide. My background in academic support and non-profit work have equipped me with strong communication, collaboration, and leadership skills. I am proficient in Spanish (native), English (fluent), and French (limited working proficiency).',
+  },
+  {
+    year: 2024,
+    title: 'General Assembly Software Engineering Bootcamp',
+    details: 'Built a foundation in programming and web development (React, Node.js, Mongoose, Express). I also honed my collaborative skills through hands-on projects and Git/GitHub version control.',
+  },
+  {
+    year: 2023, 
+    title: 'CUNY Data Analytics Foundations Course',
+    details: 'Acquired the basic skills that a data analytics professional needs to gather, prepare, analyze and display data from structured and unstructured sources. Became familiar with Python, SQL and Tableau, and learned about the industries and professions that use data analytics.',
+  },
+]
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,22 +1,6 @@
-import AboutItem from '../components/AboutItem'
 
-const data = [
-  {
-    year: 2024,
-    title: '',
-    details: 'I am interested in the intersection of technology accessibility and inclusive growth, where innovative solutions empower all users and bridge the digital divide. My background in academic support and non-profit work have equipped me with strong communication, collaboration, and leadership skills. I am proficient in Spanish (native), English (fluent), and French (limited working proficiency).',
-  },
-  {
-    year: 2024,
-    title: 'General Assembly Software Engineering Bootcamp',
-    details: 'Built a foundation in programming and web development (React, Node.js, Mongoose, Express). I also honed my collaborative skills through hands-on projects and Git/GitHub version control.',
-  },
-  {
-    year: 2023, 
-    title: 'CUNY Data Analytics Foundations Course',
-    details: 'Acquired the basic skills that a data analytics professional needs to gather, prepare, analyze and display data from structured and unstructured sources. Became familiar with Python, SQL and Tableau, and learned about the industries and professions that use data analytics.',
-  },
-]
+import AboutItem from '../components/AboutItem'
+import { jobs } from '../data/jobs-data'
 
 function About() {
   return (
@@ -27,8 +11,8 @@ function About() {
       <h1 className="text-4xl font-bold text-center text-[#001b5e]">
         <span className="sr-only">About</span> About Me
       </h1>
-      {data.map((item, idx) => (
-        <AboutItem key={idx} year={item.year} title={item.title} details={item.details} />
+      {jobs.map((item, idx) => (
+        <AboutItem key={idx} {...item} />
       ))}
     </section>
   )
