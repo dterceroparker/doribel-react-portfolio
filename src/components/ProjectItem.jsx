f858e60a7e54888b12d53b54fbfc5c7788cec4ba
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-07-04 20:51:10 -0400

    update Projects and ProjectItem components add project links update card hover add link react icon

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,25 +1,43 @@
 import { FaGithubSquare } from "react-icons/fa"
+import { IoMdOpen } from "react-icons/io"
 
-const ProjectItem = ({ img, title }) => {
+const ProjectItem = ({ img, title, githubLink, projectLink }) => {
   return (
-    <section
-      className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]"
-    >
-      <img src={img} alt={title} className="rounded-xl group-hover:opacity-10 px-2 py-2" />
-      <div
-        className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"
-      >
-        <h3 className="text-xl font-bold text-white py-4 tracking-wider text-center">
-          {title}
-        </h3>
-        {/* <p className="pb-4 pt-2 text-white text-center">React Js</p> */}
-        <div className="flex flex-col items-center">
-        <button
-          className="p-2 rounded-lg bg-gray-200 text-gray-700 font-bold cursor-pointer hover:bg-blue-200 transition duration-300 ease-in-out"
-          onClick={() => window.open("https://github.com/dterceroparker")}
-        >
-          <FaGithubSquare className='mx-auto w-5 h-5'/>
-        </button>
+    <section className="relative flex items-center justify-center h-auto w-full shadow-xl rounded-xl overflow-hidden group hover:shadow-2xl transition duration-300">
+      <div className="p-4">
+        <img
+          src={img}
+          alt={title}
+          className="object-cover w-full h-48 sm:h-64 md:h-72 transform scale-100 group-hover:scale-105 transition duration-300 rounded-xl"
+        />
+      </div>
+      <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition duration-300 bg-gradient-to-t from-gray-800 to-gray-900 bg-opacity-90 text-center">
+        <div>
+          <h3 className="text-xl font-bold text-white py-4 tracking-wider">{title}</h3>
+          <div className="flex justify-center space-x-4">
+              {githubLink && (
+                <a
+                  href={githubLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="p-2 rounded-lg bg-gray-200 text-gray-700 font-bold cursor-pointer hover:bg-blue-200 hover:text-white transition duration-300 flex items-center"
+                >
+                  <FaGithubSquare className="w-6 h-6 mr-2" />
+                  GitHub
+                </a>
+              )}
+              {projectLink && (
+                <a
+                  href={projectLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="p-2 rounded-lg bg-gray-200 text-gray-700 font-bold cursor-pointer hover:bg-blue-200 hover:text-white transition duration-300 flex items-center"
+                >
+                  <IoMdOpen className="w-6 h-6 mr-2" />
+                  Visit Project
+                </a>
+            )}
+          </div>
         </div>
       </div>
     </section>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,26 +1,26 @@
-import ProjectItem from '../components/ProjectItem'
-import petHealthFetchImg from '../assets/images/petHealthFetch.png'
-import returningHomeImg from '../assets/images/returningHomeLogo.png'
-import returningHomeQuoteImg from '../assets/images/returningHomeQuote.png'
-import pawMilyImg from '../assets/images/pawMily.png'
+import ProjectItem from '../components/ProjectItem';
+import petHealthFetchImg from '../assets/images/petHealthFetch.png';
+import returningHomeQuoteImg from '../assets/images/returningHomeQuote.png';
+import pawMilyImg from '../assets/images/pawMily.png';
 
 const Projects = () => {
+  const githubLink = "https://github.com/dterceroparker"
+
   const projectsData = [
-    { img: petHealthFetchImg, title: 'Pet HealthFetch App' },
-    { img: returningHomeImg, title: 'Returning Home Logo' },
-    { img: returningHomeQuoteImg, title: 'Returning Home' },
-    { img: pawMilyImg, title: 'PawMily' }
+    { img: petHealthFetchImg, title: 'Pet HealthFetch React App', projectLink: 'https://pet-health-fetch.netlify.app/' },
+    { img: returningHomeQuoteImg, title: 'Returning Home', projectLink: 'https://returning-home-app.netlify.app/' },
+    { img: pawMilyImg, title: 'PawMily Album', projectLink: 'https://pawmily-album.fly.dev/' }
   ]
 
   return (
-    <section id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-8">
+    <section id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-2">
       <h1 className="text-4xl font-bold text-center text-[#001b5e] mb-8">My Projects</h1>
       <p className="text-center mb-8 py-4 text-gray-500">
         My projects cater to diverse needs, from the mental health-focused Returning Home App that supports bootcamp graduates to the convenient Pet HealthFetch app for pet owners. Additionally, PawFamily explores the intersection of social interaction and a calming environment. These projects showcase my interest in the potential for technology to enhance different aspects of life. While they serve distinct purposes, there might be intriguing possibilities for combining features in the future, like incorporating pet-related content within the Returning Home App.
       </p>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
         {projectsData.map((project, idx) => (
-          <ProjectItem key={idx} {...project} />
+          <ProjectItem key={idx} {...project} githubLink={githubLink} />
         ))}
       </div>
     </section>
