dd51412465a75020e21a6049b41fa7db441a7366
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-07-06 21:01:05 -0400

    add github icon to landing component

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,15 @@
 import { TypeAnimation } from 'react-type-animation'
+import { FaGithubSquare, FaLinkedin } from "react-icons/fa"
 
 const Landing = () => {
+  const githubMainLink = 'https://github.com/dterceroparker'
+  const linkedinLink = 'https://www.linkedin.com/in/doribelt-p'
+
   return (
     <div id="main" className="relative flex items-center justify-center h-screen">
       <img
         className="absolute inset-0 w-full h-full object-cover"
-        src="/landing.jpg"
+        src="/landing2.jpeg"
         alt="landing page background"
       />
       <div className="absolute top-0 left-0 w-full h-screen bg-white/10 flex items-center justify-center">
@@ -31,13 +35,25 @@ const Landing = () => {
               style={{ fontSize: '1em', paddingLeft: '5px', display: 'inline-block' }}
             />
           </h2>
-          <div className="mt-4">
-            <div
-              className="linkedin-icon cursor-pointer bg-gradient-to-r from-blue-500 to-indigo-500 text-white bg-opacity-95 border border-linkedin hover:bg-gradient-to-r hover:from-blue-600 hover:to-indigo-600 hover:border-yellow-500 rounded-full p-1.5 transition duration-300 ease-in-out inline-flex items-center justify-center"
-              onClick={() => window.open('https://www.linkedin.com/in/doribelt-p/')}
+          <div className="mt-4 flex space-x-4 justify-center">
+            <a
+              href={linkedinLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="p-2 rounded-full bg-gradient-to-r from-blue-500 to-indigo-500 text-white font-bold cursor-pointer hover:bg-gradient-to-r hover:from-blue-600 hover:to-indigo-600 transition duration-300 flex items-center"
             >
-              <span className="text-base font-semibold">LinkedIn</span>
-            </div>
+              <FaLinkedin className="w-6 h-6 mr-2" />
+              LinkedIn
+            </a>
+            <a
+              href={githubMainLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="p-2 rounded-full bg-gray-200 text-gray-700 font-bold cursor-pointer hover:bg-black hover:text-white transition duration-300 flex items-center"
+            >
+              <FaGithubSquare className="w-6 h-6 mr-2" />
+              GitHub
+            </a>
           </div>
         </div>
       </div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,12 +4,12 @@ import returningHomeQuoteImg from '../assets/images/returningHomeQuote.png';
 import pawMilyImg from '../assets/images/pawMily.png';
 
 const Projects = () => {
-  const githubLink = "https://github.com/dterceroparker"
+  
 
   const projectsData = [
-    { img: petHealthFetchImg, title: 'Pet HealthFetch React App', projectLink: 'https://pet-health-fetch.netlify.app/' },
-    { img: returningHomeQuoteImg, title: 'Returning Home', projectLink: 'https://returning-home-app.netlify.app/' },
-    { img: pawMilyImg, title: 'PawMily Album', projectLink: 'https://pawmily-album.fly.dev/' }
+    { img: petHealthFetchImg, title: 'Pet HealthFetch React App', projectLink: 'https://pet-health-fetch.netlify.app/', githubLink: 'https://github.com/dterceroparker/pet-healthfetch-front-end'},
+    { img: returningHomeQuoteImg, title: 'Returning Home', projectLink: 'https://returning-home-app.netlify.app/', githubLink: 'https://github.com/dterceroparker/returning-home-app' },
+    { img: pawMilyImg, title: 'PawMily Album', projectLink: 'https://pawmily-album.fly.dev/', githubLink: 'https://github.com/dterceroparker/pawmily-album' }
   ]
 
   return (
@@ -20,7 +20,7 @@ const Projects = () => {
       </p>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
         {projectsData.map((project, idx) => (
-          <ProjectItem key={idx} {...project} githubLink={githubLink} />
+          <ProjectItem key={idx} {...project} />
         ))}
       </div>
     </section>
