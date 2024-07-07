31e4255ad6cd7291b2649d0b30af653ab926041f
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-07-06 21:16:20 -0400

    update landing page text accessibility

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -9,12 +9,12 @@ const Landing = () => {
     <div id="main" className="relative flex items-center justify-center h-screen">
       <img
         className="absolute inset-0 w-full h-full object-cover"
-        src="/landing2.jpeg"
+        src="/landing.jpeg"
         alt="landing page background"
       />
       <div className="absolute top-0 left-0 w-full h-screen bg-white/10 flex items-center justify-center">
-        <div className="max-w-[700px] mx-auto text-center p-4 lg:p-8 bg-white/60 rounded-lg">
-          <h1 className="text-4xl sm:text-5xl font-bold text-gray-800">
+        <div className="max-w-[700px] mx-auto text-center p-4 lg:p-8 bg-white/60 rounded-lg shadow-lg backdrop-filter backdrop-blur-md">
+          <h1 className="text-4xl sm:text-5xl font-bold text-gray-800 mb-2">
             {`Welcome! I'm Doribel!`}
           </h1>
           <h2 className="text-2xl sm:text-3xl py-4 text-gray-800">
@@ -40,7 +40,7 @@ const Landing = () => {
               href={linkedinLink}
               target="_blank"
               rel="noopener noreferrer"
-              className="p-2 rounded-full bg-gradient-to-r from-blue-500 to-indigo-500 text-white font-bold cursor-pointer hover:bg-gradient-to-r hover:from-blue-600 hover:to-indigo-600 transition duration-300 flex items-center"
+              className="p-2 rounded-full bg-gradient-to-r from-blue-500 to-indigo-500 text-white font-bold cursor-pointer hover:bg-gradient-to-r hover:from-blue-600 hover:to-indigo-600 transition duration-300 ease-in-out flex items-center shadow-lg"
             >
               <FaLinkedin className="w-6 h-6 mr-2" />
               LinkedIn
@@ -49,7 +49,7 @@ const Landing = () => {
               href={githubMainLink}
               target="_blank"
               rel="noopener noreferrer"
-              className="p-2 rounded-full bg-gray-200 text-gray-700 font-bold cursor-pointer hover:bg-black hover:text-white transition duration-300 flex items-center"
+              className="p-2 rounded-full bg-gray-200 text-gray-700 font-bold cursor-pointer hover:bg-black hover:text-white transition duration-300 ease-in-out flex items-center shadow-lg"
             >
               <FaGithubSquare className="w-6 h-6 mr-2" />
               GitHub
