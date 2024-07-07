917f21779f4b9fe9976ebb75c5e540731472bedc
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-07-06 21:55:21 -0400

    update contact component

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,32 +1,31 @@
-
 const Contact = () => {
   return (
     <div id='contact' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
       <h1 className='py-4 text-4xl font-bold text-center text-[#001b5e]'>Contact</h1>
-      <form action="https://getform.io/f/paygneea" method='POST' encType='multipart/form-data'>
+      <form action="https://getform.io/f/paygneea" method='POST' encType='multipart/form-data' className='bg-white p-8 rounded-lg shadow-lg'>
         <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
           <div className='flex flex-col'>
             <label className='uppercase text-sm py-2'>Name</label>
-            <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" name='name' />
+            <input className='border-2 rounded-lg p-3 flex border-gray-300 focus:border-[#2e65ef] focus:ring-2 focus:ring-[#2e65ef] transition duration-300' type="text" name='name' />
           </div>
           <div className='flex flex-col'>
             <label className='uppercase text-sm py-2'>Phone</label>
-            <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" name='phone' />
+            <input className='border-2 rounded-lg p-3 flex border-gray-300 focus:border-[#2e65ef] focus:ring-2 focus:ring-[#2e65ef] transition duration-300' type="text" name='phone' />
           </div>
           <div className='flex flex-col py-2'>
             <label className='uppercase text-sm py-2'>Email</label>
-            <input className='border-2 rounded-lg p-3 flex border-gray-300' type="email" name='email' />
+            <input className='border-2 rounded-lg p-3 flex border-gray-300 focus:border-[#2e65ef] focus:ring-2 focus:ring-[#2e65ef] transition duration-300' type="email" name='email' />
           </div>
           <div className='flex flex-col py-2'>
             <label className='uppercase text-sm py-2'>Subject</label>
-            <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" name='subject' />
+            <input className='border-2 rounded-lg p-3 flex border-gray-300 focus:border-[#2e65ef] focus:ring-2 focus:ring-[#2e65ef] transition duration-300' type="text" name='subject' />
           </div>
-          <div className='flex flex-col py-2'>
+          <div className='flex flex-col py-2 md:col-span-2'>
             <label className='uppercase text-sm py-2'>Message</label>
-            <textarea className='border-2 rounded-lg p-3 border-gray-300' rows='10' name='message' ></textarea>
+            <textarea className='border-2 rounded-lg p-3 border-gray-300 focus:border-[#2e65ef] focus:ring-2 focus:ring-[#2e65ef] transition duration-300' rows='10' name='message' ></textarea>
           </div>
         </div>
-        <button className='bg-[#2e65efa4] text-gray-100 mt-4 w-full p-4 rounded-lg hover:bg-[#488ceae3] hover:text-white hover:shadow-md'>
+        <button className='bg-[#2e65ef] text-white mt-4 w-full p-4 rounded-lg hover:bg-[#001b5e] hover:text-white hover:shadow-md transition duration-300'>
           Send Message
         </button>
       </form>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -14,10 +14,21 @@ const Projects = () => {
 
   return (
     <section id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-2">
-      <h1 className="text-4xl font-bold text-center text-[#001b5e] mb-8">My Projects</h1>
-      <p className="text-center mb-8 py-4 text-gray-500">
-        My projects cater to diverse needs, from the mental health-focused Returning Home App that supports bootcamp graduates to the convenient Pet HealthFetch app for pet owners. Additionally, PawFamily explores the intersection of social interaction and a calming environment. These projects showcase my interest in the potential for technology to enhance different aspects of life. While they serve distinct purposes, there might be intriguing possibilities for combining features in the future, like incorporating pet-related content within the Returning Home App.
-      </p>
+      <h1 className="text-4xl font-bold text-center text-[#001b5e] mb-8 mt-0">
+        My Projects
+      </h1>
+      <div className="bg-white p-6 rounded-lg shadow-lg">
+        <p className="text-center mb-8 py-4 text-gray-500">
+          My projects cater to diverse needs, from the mental health-focused 
+          Returning Home App that supports bootcamp graduates to the convenient 
+          Pet HealthFetch app for pet owners. Additionally, PawFamily explores 
+          the intersection of social interaction and a calming environment. These 
+          projects showcase my interest in the potential for technology to enhance 
+          different aspects of life. While they serve distinct purposes, there might 
+          be intriguing possibilities for combining features in the future, like 
+          incorporating pet-related content within the Returning Home App.
+        </p>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
         {projectsData.map((project, idx) => (
           <ProjectItem key={idx} {...project} />
@@ -25,6 +36,7 @@ const Projects = () => {
       </div>
     </section>
   )
+  
 }
 
 export default Projects
