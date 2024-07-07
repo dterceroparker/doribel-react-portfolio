e9a3e6fd3ee6c1ce17af8bec7d284b78a114db28
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-07-07 11:55:44 -0400

    Projects css update

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -13,20 +13,13 @@ const Projects = () => {
   ]
 
   return (
-    <section id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-2">
+    <section id="projects" className="max-w-screen-lg mx-auto md:pl-20 p-4 py-8">
       <h1 className="text-4xl font-bold text-center text-[#001b5e] mb-8 mt-0">
         My Projects
       </h1>
-      <div className="bg-white p-6 rounded-lg shadow-lg">
-        <p className="text-center mb-8 py-4 text-gray-500">
-          My projects cater to diverse needs, from the mental health-focused 
-          Returning Home App that supports bootcamp graduates to the convenient 
-          Pet HealthFetch app for pet owners. Additionally, PawFamily explores 
-          the intersection of social interaction and a calming environment. These 
-          projects showcase my interest in the potential for technology to enhance 
-          different aspects of life. While they serve distinct purposes, there might 
-          be intriguing possibilities for combining features in the future, like 
-          incorporating pet-related content within the Returning Home App.
+      <div className="bg-white p-6 rounded-lg shadow-lg mb-8">
+        <p className="text-lg text-gray-700 leading-relaxed">
+          My projects cater to diverse needs, from the mental health-focused Returning Home App that supports bootcamp participants to the convenient Pet HealthFetch app for pet owners. Additionally, PawFamily explores the intersection of social interaction and a calming environment. These projects showcase my interest in the potential for technology to enhance different aspects of life. While they serve distinct purposes, there are possibilities for combining features in the future, such as incorporating pet-related content within the Returning Home App.
         </p>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
@@ -36,7 +29,6 @@ const Projects = () => {
       </div>
     </section>
   )
-  
 }
 
 export default Projects
